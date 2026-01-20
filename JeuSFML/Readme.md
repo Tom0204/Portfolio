e5372bd5f5486efd04be5fafca38c66dc0b3ecea
Brondel Tom <tomaurelien.brondel@gmail.com>
2026-01-20 21:34:12 +0100

    Setup du projet fini, ajout du readme et du gitignore

---
+++ JeuSFML/Readme.md
@@ -0,0 +1,13 @@
+﻿# JeuSFML - Portfolio C++/SFML/CMake
+
+## Prérequis
+- Visual Studio 2022 (Desktop C++)
+- CMake 3.20+
+- SFML 2.6.2 (x64) : https://www.sfml-dev.org/download/sfml/2.6.2/
+
+## Installation
+1. Clone : `git clone https://github.com/tonpseudo/JeuSFML`
+2. SFML → `C:\Libs\SFML-2.6.2`
+3. VS2022 → Ouvrir dossier → sélectionner preset x64-debug-sfml
+4. Copier DLL depuis `SFML/bin/` vers dossier build
+5. F5 !
---
+++ JeuSFML/src/JeuSFML.cpp
@@ -0,0 +1,26 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "JeuSFML");
+
+    sf::RectangleShape rect(sf::Vector2f(100.f, 100.f));
+    rect.setFillColor(sf::Color::Red);
+    rect.setPosition(350.f, 250.f); // à peu près au centre
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+        window.draw(rect);
+        window.display();
+    }
+
+    return 0;
+}
